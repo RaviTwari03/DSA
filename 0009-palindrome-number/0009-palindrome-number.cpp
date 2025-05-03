ce8804a6589c5f398ba1d315f1986ac7d1ffc40c
RaviTwari03 <ravirkt0@gmail.com>
2025-05-03 20:26:08 +0530

    Time: 3 ms (46.9%), Space: 8.7 MB (43.51%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0 itself) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even length, x == reversed; for odd length, x == reversed/10
+        return x == reversed || x == reversed / 10;
+    }
+};
