28b2bd393bb3fb702e916f69b9bf7c83108ae141
RaviTwari03 <ravirkt0@gmail.com>
2025-05-07 12:50:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (14.02%) - LeetHub

---
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string defangIPaddr(string s) {
+       int start = 0; 
+       int end = s.size();
+       int index;
+        string result;
+        
+    while ( start < end){
+        if(s[start]=='.'){
+            result += "[.]";
+        }else{
+            result += s[start];
+        }
+            start++;
+
+        
+    }
+    return result;
+}
+};
