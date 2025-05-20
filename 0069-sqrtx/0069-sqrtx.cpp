80539da0926fb49b56c1027793bd5464864b87ad
RaviTwari03 <ravirkt0@gmail.com>
2025-05-20 10:35:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.89%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+     if (x < 2) return x;  // Handle 0 and 1 directly
+
+        
+        int low=0;
+        int high=x/2+1;
+        int res=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long square=(long)mid*mid;
+            if(square==x){// in case of perfect square
+                res=mid;
+                return res;
+            }else if(square<x){
+                res=mid;
+                low=mid+1;
+            }else{
+                
+                high=mid-1;
+            }
+        }
+        return res;
+    }   
+    
+};
