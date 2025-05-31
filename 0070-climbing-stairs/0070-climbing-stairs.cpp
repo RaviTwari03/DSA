f27974bbc318151f2135b4d324739be5bc4656fa
RaviTwari03 <ravirkt0@gmail.com>
2025-05-31 19:18:30 +0530

    Time: 1 ms (6.61%), Space: 8.5 MB (34.82%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return helper(n, memo);
+    }
+
+    int helper(int n, vector<int>& memo) {
+        if (n == 0 || n == 1)
+            return 1;
+        if (memo[n] != -1)
+            return memo[n];
+        return memo[n] = helper(n - 1, memo) + helper(n - 2, memo);
+    }
+};
