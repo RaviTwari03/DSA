790662ae5cca5c95138dcaa38db911990c81bd01
RaviTwari03 <ravirkt0@gmail.com>
2025-06-06 12:33:28 +0530

    Time: 2 ms (38.42%), Space: 11.9 MB (17.49%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
 
-    void permut(vector<int>nums,vector<vector<int>>&ans,vector<int>&temp,vector<bool>&visited){
+    void permut(vector<int>nums,vector<vector<int>>&ans,int index){
         
         // Base condition
-        if(visited.size()==temp.size()){
-            ans.push_back(temp);
+        // if(visited.size()==temp.size()){
+        //     ans.push_back(temp);
+        //     return;
+        // }
+
+        if(index==nums.size()){
+            ans.push_back(nums);
             return;
         }
 
-        for(int i=0; i < visited.size();i++){
-            if(visited[i]==0){
-                visited[i]=1;
-                temp.push_back(nums[i]);
-                permut(nums,ans,temp,visited);
-                visited[i]=0;
-                temp.pop_back();
-            }
+        // for(int i=0; i < visited.size();i++){
+        //     if(visited[i]==0){
+        //         visited[i]=1;
+        //         temp.push_back(nums[i]);
+        //         permut(nums,ans,temp,visited);
+        //         visited[i]=0;
+        //         temp.pop_back();
+        //     }
+        // }
+    //     for(int i = index; i < nums.size(); i++){}
+    //     swap(nums[i],nums[index]);
+    //     permut(nums,ans,index+1);
+    //     swap(nums[i],nums[index]);
+    // }
+    for (int i = index; i < nums.size(); i++) {
+            swap(nums[i], nums[index]);
+            permut(nums, ans, index + 1);
+            swap(nums[i], nums[index]); // backtrack
         }
     }
 
@@ -26,7 +41,7 @@ public:
         vector<vector<int>>ans;
         vector<int>temp;
         vector<bool>visited(nums.size(),0);
-        permut(nums,ans,temp,visited);
+        permut(nums,ans,0);
         return ans;
     }
 };
