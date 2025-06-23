4e4f709a82a7eb6bb82e58c0e6ac80351cd7354a
RaviTwari03 <ravirkt0@gmail.com>
2025-06-23 23:59:44 +0530

    Time: 10 ms (47.1%), Space: 11.2 MB (83.1%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -14,13 +14,11 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        // Step 1: Detect if a cycle exists
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
 
             if (slow == fast) {
-                // Step 2: Find the start of the cycle
                 ListNode* entry = head;
                 while (entry != slow) {
                     entry = entry->next;
@@ -30,7 +28,6 @@ public:
             }
         }
 
-        // No cycle found
         return NULL;
     }
 };
