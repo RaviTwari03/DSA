f8d815d78fdb6f99877806ad0e06f8591badc17f
RaviTwari03 <ravirkt0@gmail.com>
2025-06-29 17:04:16 +0530

    Time: 19 ms (75.1%), Space: 81.4 MB (63.59%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,37 +1,65 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        stack<int> st;
-        heights.push_back(0); // Sentinel
-        int maxArea = 0;
+    int largestRectangleArea(vector<int>& ht) {
+    //     stack<int> st;
+    //     heights.push_back(0); 
+    //     int maxArea = 0;
 
-        for (int i = 0; i < heights.size(); ++i) {
-            while (!st.empty() && heights[i] < heights[st.top()]) {
-                int height = heights[st.top()];
+    //     for (int i = 0; i < heights.size(); ++i) {
+    //         while (!st.empty() && heights[i] < heights[st.top()]) {
+    //             int height = heights[st.top()];
+    //             st.pop();
+    //             int width = st.empty() ? i : i - st.top() - 1;
+    //             maxArea = max(maxArea, height * width);
+    //         }
+    //         st.push(i);
+    //     }
+
+    //     return maxArea;
+    // }
+
+    // int maximalRectangle(vector<vector<char>>& matrix) {
+    //     if (matrix.empty() || matrix[0].empty()) return 0;
+
+    //     int n = matrix[0].size();
+    //     vector<int> heights(n, 0);
+    //     int maxRect = 0;
+
+    //     for (const auto& row : matrix) {
+    //         for (int i = 0; i < n; ++i) {
+    //             heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+    //         }
+    //         maxRect = max(maxRect, largestRectangleArea(heights));
+    //     }
+
+    //     return maxRect;
+   int ans = 0;
+        stack<int> st;
+        int n = ht.size();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && ht[st.top()] > ht[i]) {
+                int index = st.top();
                 st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                maxArea = max(maxArea, height * width);
+                if (!st.empty()) {
+                    ans = max(ans, ht[index] * (i - st.top() - 1));
+                } else {
+                    ans = max(ans, ht[index] * i);
+                }
             }
             st.push(i);
         }
 
-        return maxArea;
-    }
-
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return 0;
-
-        int n = matrix[0].size();
-        vector<int> heights(n, 0);
-        int maxRect = 0;
-
-        for (const auto& row : matrix) {
-            for (int i = 0; i < n; ++i) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+        while (!st.empty()) {
+            int index = st.top();
+            st.pop();
+            if (!st.empty()) {
+                ans = max(ans, ht[index] * (n - st.top() - 1));
+            } else {
+                ans = max(ans, ht[index] * n);
             }
-            maxRect = max(maxRect, largestRectangleArea(heights));
         }
 
-        return maxRect;
+        return ans;
     }
 };
