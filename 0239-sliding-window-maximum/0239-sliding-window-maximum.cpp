f3e1dd5f1cb415c3d9d3b3da9307c7dd85339f9e
RaviTwari03 <ravirkt0@gmail.com>
2025-07-02 13:13:52 +0530

    Time: 15 ms (89.97%), Space: 139.1 MB (82.85%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,61 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; 
-        vector<int> result;
+        // deque<int> dq; 
+        // vector<int> result;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (!dq.empty() && dq.front() <= i - k) {
-                dq.pop_front();
-            }
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     if (!dq.empty() && dq.front() <= i - k) {
+        //         dq.pop_front();
+        //     }
 
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
+        //     while (!dq.empty() && nums[dq.back()] < nums[i]) {
+        //         dq.pop_back();
+        //     }
 
            
-            dq.push_back(i);
+        //     dq.push_back(i);
+
+        //     if (i >= k - 1) {
+        //         result.push_back(nums[dq.front()]);
+        //     }
+        // }
+
+        // return result;
+       
+    // vector<int> ans;
+    // int n = nums.size();
+    
+    // for (int i = 0; i <= n - k; i++) {   // i goes from 0 to n-k
+    //     int total = INT_MIN;
+    //     for (int j = i; j < i + k; j++) { // j runs from i to i+k-1
+    //         total = max(total, nums[j]);
+    //     }
+    //     ans.push_back(total);
+    // }
+
+    // return ans;
+
+     deque<int> d;
+        vector<int> ans;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (!d.empty() && d.front() <= i - k) {
+                d.pop_front();
+            }
+
+            while (!d.empty() && nums[i] > nums[d.back()]) {
+                d.pop_back();
+            }
+
+            d.push_back(i);
 
             if (i >= k - 1) {
-                result.push_back(nums[dq.front()]);
+                ans.push_back(nums[d.front()]);
             }
         }
 
-        return result;
+        return ans;
     }
 };
