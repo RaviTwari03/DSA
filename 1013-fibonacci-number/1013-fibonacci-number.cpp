26995823eaee6d7ac83c440719d7ff33d4cdca29
RaviTwari03 <ravirkt0@gmail.com>
2025-07-15 01:07:37 +0530

    Time: 2 ms (77.54%), Space: 7.9 MB (36.62%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -10,11 +10,38 @@ public:
         }
 
 
+    // int fib(int n) {
+    //     // if(n<=1) return n;
+    //     // return (fib(n-1)+fib(n-2));
+    //     vector<int>dp(n+1,-1);
+    //     return fibon(n,dp);
+    // }
+
+// Bottom Down Approach
+
+    // int fib(int n) {
+    //     if(n<=1) return n;
+        
+    //     vector<int>dp(n+1,-1);
+    //     dp[0]=0;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i] = dp[i-1]+dp[i -2];
+    //     }
+    //     return dp[n];
+    // }
+
+    // Space optimisation
     int fib(int n) {
-        // if(n<=1) return n;
-        // return (fib(n-1)+fib(n-2));
-        vector<int>dp(n+
-        1,-1);
-        return fibon(n,dp);
+        if(n<=1) return n;
+        
+        int curr, prev = 1, prev1 = 0;
+
+        for(int i = 2;i<=n;i++){
+            curr=prev+prev1;
+            prev1=prev;
+            prev=curr;
+        }
+        return curr;
     }
 };
