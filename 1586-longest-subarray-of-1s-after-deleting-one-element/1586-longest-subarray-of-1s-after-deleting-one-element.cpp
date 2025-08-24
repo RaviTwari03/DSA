6680ba1334d94fbb65055e8d52d5c190d2f3cc08
Ravi Tiwari <ravirkt0@gmail.com>
2025-08-24 07:48:04 +0530

    Time: 0 ms (100%), Space: 60 MB (85.53%) - LeetHub

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    // int n;
+    
+    // int findMax(vector<int>& nums, int skip_idx) {
+        
+    //     int currLength = 0;
+    //     int maxLength  = 0;
+        
+    //     for(int i = 0; i<n; i++) {
+    //         if(i == skip_idx)
+    //             continue;
+            
+    //         if(nums[i] == 1) {
+    //             currLength++;
+    //             maxLength = max(maxLength, currLength);
+    //         } else {
+    //             currLength = 0;
+    //         }
+    //     }
+        
+    //     return maxLength;
+        
+    // }
+    
+    // int longestSubarray(vector<int>& nums) {
+    //     n = nums.size();
+    //     int result = 0;
+        
+    //     int countZero = 0;
+    //     for(int i = 0; i<n; i++) {
+    //         if(nums[i] == 0) {
+    //             countZero++;
+    //             result = max(result, findMax(nums, i));
+    //         }
+            
+    //     }
+        
+    //     if(countZero == 0)
+    //         return n-1;
+        
+    //     return result;
+        
+    // }
+   
+    int longestSubarray(vector<int>& nums) {
+
+        int zeroCount = 0;
+        int longestWindow = 0;
+
+        int i = 0;
+        
+        for (int j = 0; j < nums.size(); j++) {
+            zeroCount += (nums[j] == 0);
+                          
+            // Shrink the window until the zero counts come under the limit.
+            while (zeroCount > 1) {
+                zeroCount -= (nums[i] == 0);
+                i++;
+            }
+              
+            longestWindow = max(longestWindow, j - i);
+        }
+
+        return longestWindow;
+    }
+};
+
+
