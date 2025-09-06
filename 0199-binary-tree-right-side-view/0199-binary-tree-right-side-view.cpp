b650c43ce372bf4e08481311309d9dd798712610
Ravi Tiwari <ravirkt0@gmail.com>
2025-09-06 10:44:19 +0530

    Time: 0 ms (100%), Space: 15 MB (60.76%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,32 +11,54 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        if(!root) {
-            return {};
-        }
-    queue<TreeNode*> que;
-    vector<int>ans;
 
-    que.push(root);
+    void preOrder(TreeNode* root, int level, vector<int>&ans){
+        if(!root) return;
 
-    while(!que.empty()){
-        int n=que.size();
-        TreeNode* rightNode = NULL;
+        if(ans.size()<level){
+            ans.push_back(root->val);
+        }
 
-        while(n--){
-            rightNode = que.front();
-            que.pop();
-            if(rightNode->left){
-                que.push(rightNode->left);
-            }
-            if(rightNode->right){
-                que.push(rightNode->right);
-            }
-        }
-            ans.push_back(rightNode->val);
-        }
-        return ans;
+        preOrder(root->right,level+1,ans);
+        preOrder(root->left,level+1,ans);
     }
 
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        preOrder(root,1,ans);
+        return ans;
+    }
 };
+
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(!root) {
+//             return {};
+//         }
+//     queue<TreeNode*> que;
+//     vector<int>ans;
+
+//     que.push(root);
+
+//     while(!que.empty()){
+//         int n=que.size();
+//         TreeNode* node = NULL;
+
+//         while(n--){
+//             node = que.front();
+//             que.pop();
+//             if(node->left){
+//                 que.push(node->left);
+//             }
+//             if(node->right){
+//                 que.push(node->right);
+//             }
+//         }
+//             ans.push_back(node->val);
+//         }
+//         return ans;
+//     }
+
+// };
