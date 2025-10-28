07d92ac6c9181dda8b12f9606e09722a34a94b41
Ravi Tiwari <ravirkt0@gmail.com>
2025-10-29 00:30:49 +0530

    Time: 0 ms (100%), Space: 95 MB (80.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int m = nums1.size();
-        int n = nums2.size();
-        
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1); // always binary search on smaller array
+
+        int m = nums1.size(), n = nums2.size();
         int low = 0, high = m;
-        while(low <= high) {
-            
-            int Px = low + (high-low)/2;
-            int Py = (m+n+1)/2 - Px;
-            
-            int x1  = (Px == 0) ? INT_MIN : nums1[Px-1];
-            int x3 = (Px == m) ? INT_MAX : nums1[Px];
-            
-            int x2  = (Py == 0) ? INT_MIN : nums2[Py-1];
-            int x4 = (Py == n) ? INT_MAX : nums2[Py];
-            
-            if(x1 <= x4 && x2 <= x3) {
-                if((m+n)%2 == 0)
-                    return (max(x1, x2) + min(x3, x4))/2.0;
-                
-                return max(x1, x2);
-            } else if(x1 > x4) {
-                high = Px-1;
-            } else {
-                low = Px+1;
+
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (m + n + 1) / 2 - i;
+
+            int L1 = (i == 0) ? INT_MIN : nums1[i - 1];
+            int R1 = (i == m) ? INT_MAX : nums1[i];
+            int L2 = (j == 0) ? INT_MIN : nums2[j - 1];
+            int R2 = (j == n) ? INT_MAX : nums2[j];
+
+            if (L1 <= R2 && L2 <= R1) {
+                if ((m + n) % 2 == 0)
+                    return (max(L1, L2) + min(R1, R2)) / 2.0;
+                else
+                    return max(L1, L2);
             }
+            else if (L1 > R2)
+                high = i - 1;
+            else
+                low = i + 1;
         }
-        
-        return -1;
+
+        return 0.0; // shouldn't reach here
     }
 };
