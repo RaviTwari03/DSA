1ba6f4c7bb79a91bd8161da1f3a68dc83f1bc3c7
Ravi Tiwari <ravirkt0@gmail.com>
2025-10-31 12:25:32 +0530

    Time: 2 ms (39.36%), Space: 9.8 MB (94.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-        void subsequence(vector<int>& nums,int index,int n,vector<vector<int>>&ans,vector<int>&temp){
-            
-            if(index==n){
-                ans.push_back(temp);
-                return;
-            }
-
-            subsequence(nums,index+1,n,ans,temp);
-            temp.push_back(nums[index]);
-            subsequence(nums,index+1,n,ans,temp);
-            temp.pop_back();
-        }
-
-
-
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int>temp;
-        subsequence(nums,0,nums.size(),ans,temp);
+        vector<vector<int>> result;
+        vector<int> current;
+        backtrack(0, nums, current, result);
+        return result;
+    }
 
-        return ans;
+private:
+    void backtrack(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
+        result.push_back(current);
+
+        for (int i = index; i < nums.size(); ++i) {
+            current.push_back(nums[i]);           // choose
+            backtrack(i + 1, nums, current, result); // explore
+            current.pop_back();                   // un-choose
+        }
     }
 };
