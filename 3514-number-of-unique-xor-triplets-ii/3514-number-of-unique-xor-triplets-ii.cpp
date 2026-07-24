e049c96e54f3847bd7492a4217652761234d1721
Ravi Tiwari <ravirkt0@gmail.com>
2026-07-24 13:00:36 +0530

    Time: 2043 ms (39.33%), Space: 98.6 MB (15.73%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -3,7 +3,6 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
 
-        //Store all Pair Xors
         unordered_set<int> s1;
         for(int i = 0; i < n; i++) {
             for(int j = i; j < n; j++) {
@@ -11,7 +10,6 @@ public:
             }
         }
 
-        //Find triplet xors
         unordered_set<int> s2;
         for(int pairXor : s1) {
             for(int &num : nums) {
