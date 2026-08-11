ec2a09565ab32630560b578354ee4b0cbc65d607
Ravi Tiwari <ravirkt0@gmail.com>
2026-08-11 12:33:16 +0530

    Time: 3 ms (23.9%), Space: 23.1 MB (29.32%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -3,7 +3,6 @@ public:
     int missingInteger(vector<int>& nums) {
         int sum = nums[0];
 
-        // Find the sequential prefix.
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] == nums[i - 1] + 1)
                 sum += nums[i];
@@ -11,10 +10,8 @@ public:
                 break;
         }
 
-        // Store all elements for O(1) average lookup.
         unordered_set<int> seen(nums.begin(), nums.end());
 
-        // Find the smallest missing integer >= prefix sum.
         while (seen.count(sum))
             sum++;
 
