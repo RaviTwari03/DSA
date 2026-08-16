48feaf7facec3c417b1e62e72d64d4fb643eb9fe
Ravi Tiwari <ravirkt0@gmail.com>
2026-08-16 11:17:45 +0530

    Time: 8 ms (30.2%), Space: 131.3 MB (33.5%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.cpp
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -7,19 +7,13 @@ public:
             cnt[x % 3]++;
         }
 
-        // CHANGED:
-        // Handle the two cases based on the parity of stones
-        // whose remainder is 0.
+        
         if (cnt[0] % 2 == 0) {
-            // With an even number of 0-mod-3 stones,
-            // Alice needs at least one stone from each group.
+            
             return cnt[1] > 0 && cnt[2] > 0;
         }
 
-        // CHANGED:
-        // When cnt[0] is odd, Alice wins if one of the
-        // non-zero remainder groups has at least two more
-        // stones than the other.
+       
         return abs(cnt[1] - cnt[2]) > 2;
     }
 };
