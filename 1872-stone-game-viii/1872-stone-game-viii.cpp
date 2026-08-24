f012bfbb73d745faab8093949558b1b4678fdfc8
Ravi Tiwari <ravirkt0@gmail.com>
2026-08-24 10:42:44 +0530

    Time: 107 ms (58.71%), Space: 87.1 MB (92.39%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+        // Prefix sum
+        for (int i = 1; i < n; i++) {
+            stones[i] += stones[i - 1];
+        }
+
+        // Start from the last possible state
+        int dp = stones[n - 1];
+
+        // Work backwards
+        for (int i = n - 2; i > 0; i--) {
+            dp = max(dp, stones[i] - dp);
+        }
+
+        return dp;
+    }
+};
